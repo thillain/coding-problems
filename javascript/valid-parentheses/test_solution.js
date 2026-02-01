47d6d7a13221347a90be58e998dde47e412d32ee
Thillai Narayanan <judearasu@gmail.com>
2026-02-01 14:20:33 -0500

    fix(stack): LEET-5 20. Valid Parentheses
    javascript - comparison to the map on the if block not needed.

---
+++ javascript/valid-parentheses/test_solution.js
@@ -0,0 +1,21 @@
+const { isValid } = require("./solution");
+
+test("basic test", () => {
+    expect(isValid('()')).toEqual(true);
+});
+
+test('test two', () => {
+    expect(isValid('()[]{}')).toEqual(true);
+})
+
+test('test three', () => {
+    expect(isValid('(]')).toEqual(false);
+})
+
+test('test four', () => {
+    expect(isValid('([])')).toEqual(true);
+})
+
+test('test five', () => {
+    expect(isValid('([)]')).toEqual(false);
+})
