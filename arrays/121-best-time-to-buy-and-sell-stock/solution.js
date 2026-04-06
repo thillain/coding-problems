36a3887430afdfbb1b7a517a9e230ffd5c56346f
Thillai N <judearasu@gmail.com>
2026-04-05 21:23:13 -0400

    feat(121-best-time-to-buy-and-sell-stock): implement maxProfit function to calculate maximum stock profit

---
+++ arrays/121-best-time-to-buy-and-sell-stock/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+
+    let minPrice = Infinity;
+
+    let maxProfit = 0;
+
+    for (let price of prices) {
+
+        if (price < minPrice) {
+            minPrice = price;
+        } else if ((price - minPrice) > maxProfit) {
+            maxProfit = price - minPrice;
+        }
+
+    }
+    return maxProfit;
+};
+
+
+console.log(maxProfit(prices = [7, 1, 5, 3, 6, 4]));
